df0849e88eeffba1d26f530cbe0f542638f6ec81
Maks <93095596+MaksShcherban@users.noreply.github.com>
2024-04-25 17:53:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,20 @@
 const select = document.querySelector("#earth");
 
-const img_one = document.querySelector(".imgs_1");
-const img_two = document.querySelector(".imgs_2");
-const img_three = document.querySelector(".imgs_3");
-const img_four = document.querySelector(".imgs_4");
-const img_five = document.querySelector(".imgs_5");
+const images = {
+  144: document.querySelector(".imgs_1"),
+  240: document.querySelector(".imgs_2"),
+  480: document.querySelector(".imgs_3"),
+  720: document.querySelector(".imgs_4"),
+  1080: document.querySelector(".imgs_5"),
+};
 
 select.addEventListener("change", () => {
-  if (select.value == 144) {
-    img_one.hidden = false;
-    img_two.hidden = true;
-    img_three.hidden = true;
-    img_four.hidden = true;
-    img_five.hidden = true;
-  } else if (select.value == 240) {
-    img_one.hidden = true;
-    img_two.hidden = false;
-    img_three.hidden = true;
-    img_four.hidden = true;
-    img_five.hidden = true;
-  } else if (select.value == 480) {
-    img_one.hidden = true;
-    img_two.hidden = true;
-    img_three.hidden = false;
-    img_four.hidden = true;
-    img_five.hidden = true;
-  } else if (select.value == 720) {
-    img_one.hidden = true;
-    img_two.hidden = true;
-    img_three.hidden = true;
-    img_four.hidden = false;
-    img_five.hidden = true;
-  } else if (select.value == 1080) {
-    img_one.hidden = true;
-    img_two.hidden = true;
-    img_three.hidden = true;
-    img_four.hidden = true;
-    img_five.hidden = false;
+  const selectedValue = parseInt(select.value);
+  for (const size in images) {
+    if (selectedValue === parseInt(size)) {
+      images[size].hidden = false;
+    } else {
+      images[size].hidden = true;
+    }
   }
 });
-
-console.log(select);
